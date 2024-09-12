bbfd95ba90a17ff23944fdd8bcc86b96699c4274
Malo <meunier.malo03@gmail.com>
2024-09-12 16:44:41 +0200

    flask ouais

--- flask/app/routes.py
+++ flask/app/routes.py
@@ -1,8 +1,15 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
-@app.route('/index')
+def home():
+    return render_template('home.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/index')
 def index():
     srtResult='ATTENTION TEST'
     return srtResult
